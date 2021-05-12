e4ac0152c2801aee1f1b14ab4fabc8a2346b357e
Lan Nagashima <Lan@Lans-MacBook-Pro.local>
2021-05-11 19:52:01 -0700

    update

--- main.py
+++ main.py
@@ -7,5 +7,17 @@ app.secret_key = b'xx'
 conn = mysql.connector.connect()
 
 @app.route('/')
+def first():
+    return render_template("index.html")
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        session['password'] = request.form['password']
+    return render_template("dashboard.html")
+@app.route('/dashboard',methods = ['POST','GET'])
+def dashboard():
+    return render_template('dashboard.html')
+        
 
 app.run(host = '0.0.0.0', port = 8080)
